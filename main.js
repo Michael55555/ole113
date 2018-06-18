13a2b82ed2fba61ef107f2472c38ae710d11fdf2
Ole113 <36930869+Ole113@users.noreply.github.com>
2018-06-18 11:14:43 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 window.onload = function(){
-	$("#bar, #error1, #error2,#back, #top,#homet,#blogt, #codet").hide();
+	$("#bar, #error1, #error2,#back, #top,#homet,#projectt, #codet,.div, #settings, #settingst").hide();
 	var back = document.getElementById("back");
 	back.addEventListener("click", function(){
 	$("#back, #error1,#error2").hide();
@@ -9,11 +9,25 @@ window.onload = function(){
 	var desktop = document.getElementById("desktop");
 	var footer = document.getElementById("footer");
 	var bar = document.getElementById("bar");
+	var sback = document.getElementById("sback");
+	sback.addEventListener("click", function(){
+		if(window.innerWidth > 800){
+			desktop.render();
+		}else{
+			mobile.render();
+		}
+	});
+	var settings = document.getElementById("settings");
+	settings.addEventListener("click", function(){
+		$("#homet, #bar, #top, #projectt, #codet, #settings").hide();
+		$("#settingst, #sback").show();
+		document.body.style.backgroundColor = "#212121";
+	});
 	mobile.addEventListener("click", 
 function(){
 	mobile.render();
 	if(window.innerWidth > 800){
-		$("#bar, #top, #homet").hide();
+		$("#bar, #top, #homet, #settings").hide();
 		$("#error2,#back").show();
 		document.body.style.backgroundColor = "#212121";
 	}
@@ -21,14 +35,14 @@ function(){
 	desktop.addEventListener("click", function(){
 	desktop.render();
 		if(window.innerWidth < 800){
-		$("#bar,#top, #homet").hide();
+		$("#bar,#top, #homet, #settings").hide();
 		$("#error1,#back").show();
-        document.body.style.backgroundColor = "#212121";
+		document.body.style.backgroundColor = "#212121";
 	}
 });
 	var codes = document.getElementById("codes");
 	var home = document.getElementById("home");
-	var blog = document.getElementById("blog");
+	var blog = document.getElementById("project");
 	var top = document.getElementById("top");
 	codes.addEventListener("click", function(){
 	top.innerHTML = "CODES";
@@ -36,29 +50,29 @@ function(){
 	codes.style.color = "#999999";
 	blog.style.color = "white";
 	$("#codet").show();
-	$("#homet, #blogt").hide();
+	$("#homet, #projectt").hide();
 });
 	home.addEventListener("click", function(){
 	top.innerHTML = "HOME";
 	home.style.color = "#999999";
 	codes.style.color = "white";
-	blog.style.color = "white";
+	project.style.color = "white";
 	$("#homet").show();
-	$("#blogt, #codet").hide();
+	$("#projectt, #codet").hide();
 });
-	blog.addEventListener("click", function(){
-	top.innerHTML = "BLOG";
+	project.addEventListener("click", function(){
+	top.innerHTML = "PROJECTS";
 	home.style.color = "white";
 	codes.style.color = "white";
-	blog.style.color = "#999999";
-	$("#blogt").show();
+	project.style.color = "#999999";
+	$("#projectt").show();
 	$("#homet, #codet").hide();
 });
 	var height = window.innerHeight - 58;
 	function Mobile(){
 		this.render = function(){
-		$("#mobile,#desktop,#q, #footer").hide();
-		$("#bar,#top").show();
+		$("#mobile,#desktop,#q, #footer, #sback").hide();
+		$("#bar,#top, #settings").show();
 		document.body.style.backgroundColor = "#f2f2f2";
 		/* Browser detection from Adam Khoury on youtube*/
 		var ba = ["Chrome", "Firefox", "Safari", "Opera", "MSIE", "Trident", "Edge"];
@@ -80,20 +94,16 @@ function(){
 			}
 		$("#homet").show();
 		home.style.color = "#999999";
-		document.getElementById("profile_pic").style.marginTop = "-5%";
+		document.getElementById("profile_pic").style.marginTop = "-4.7%";
 		};
 	}
 	function Desktop(){
 		this.render = function(){
 			document.body.style.backgroundColor = "#f2f2f2";
-			$("#mobile,#desktop,#q, #footer").hide();
-			$("#bar,#top, #homet").show();
+			$("#mobile,#desktop,#q, #footer, #sback").hide();
+			$("#bar,#top, #homet, #settings").show();
 			document.getElementById("profile_pic").style.marginTop = "-1.8%";
 			document.getElementById("htitle").style.marginTop = "-45%";
-			if(window.innerWidth >= 1280){
-				document.getElementById("profile_pic").style.marginTop = "-0.9%";
-				document.getElementById("htitle").style.fontSize = "50px";
-			}
 		};
 	}
 	if(window.innerWidth > 800){
