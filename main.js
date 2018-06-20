236a4a5595fdb381ac7b3373b61486352e3f3df6
Ole113 <36930869+Ole113@users.noreply.github.com>
2018-06-20 09:59:47 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,211 +1,116 @@
-body{
-	border: 0;
-	margin: 0;
-    font-family: 'Roboto Mono', monospace;
-}
-.div{
-	margin: 0 auto;
-	width: 50px;
-	margin-top: 50%;
-}
-.d1,.d2,.d3{
-	position: relative;
-	width:15px;
-	height:15px;
-	border-radius:50px;
-	animation: jump 3s infinite;
-}
-.d1{
-	background-color:#1a1aff;
-	top:25px;
-}
-.d2{
-	background-color: #1a75ff;
-	animation-delay:0.2s;
-	left:20px;
-	top:10px;
-
-}
-.d3{
-	background-color: #4d88ff;
-	animation-delay: 0.5s;
-	left:40px;
-	bottom: 5px;
-}
-@keyframes jump{
-	25%{
-		opacity: 1;
-		transform: translateY(0px);
-	}
-	50%{
-		opacity: 0.75;
-		transform: translateY(0px);
-	}
-	75%{
-		opacity: 0.50;
-		transform: translateY(0px);
-	}
-	25%{
-		opacity: 0.25;
-		transform: translateY(0px);
-	}
-}
-#loading{
-	margin: -15px;
-	margin-left: -8px;
-	font-family: 'Encode Sans Semi Expanded', sans-serif;
-	font-size: 12px;
-	color: #212121;
-}
-#bar{
-	background-color: #1a1a1a;
-	color: white;
-	width: 100%;
-	height: 45px;
-	font-size: 10px;
-	position: fixed;
-	padding: 5px;
-    border-top: #0d0d0d;
-    /*
-    -webkit-backface-visibility: hidden;
-    backface-visibility: hidden;
-    */
-    -webkit-transform: translate3d(0,0,0);
-    transform: translate3d(0,0,0);
-}
-.bard{
-	text-decoration: none;
-    color: white;
-    /*
-    -webkit-backface-visibility: hidden;
-    backface-visibility: hidden;
-    */
-    -webkit-transform: translate3d(0,0,0);
-    transform: translate3d(0,0,0);
-}
-#top{
-	text-align: center;
-	background-color: #1a1a1a;
-	width: 100%;
-	height: 35px;
-	margin-top: 0;
-	border-bottom: 1px #0d0d0d;
-	color: white;
-	padding-top: 10px;
-}
-#htitle{
-	color: white;
-	text-align: center;
-	font-size: 25px;
-	z-index: 1;
-	margin-top: -45%;
-}
-#profile_pic{
-	display: block;
-	margin-left: auto;
-	margin-right: auto;
-	padding-bottom: 10px;
-	margin-top: -2.7%;
-	width: 100%;
-    height: 100%;
-    /*
-    -webkit-backface-visibility: hidden;
-    backface-visibility: hidden;
-    */
-    -webkit-transform: translate3d(0,0,0);
-    transform: translate3d(0,0,0);
-}
-.hrl{
-	color: white;
-	width: 90%;
-}
-.hrs{
-	color: white;
-	width: 100%;
-}
-#projectt{
-	color: black;
-	text-align: center;
-}
-#ctitle{
-	color: black;
-	text-align: center;
-	font-size: 15px;
-}
-#ctitleg, #ctitleg1{
-	color: black;
-	text-align: center;
-}
-#projectt{
-	color: black;
-	text-align: left;
-	margin-left: 8px;
-}
-#ptitle{
-	text-align: center;
-}
-#hmain{
-	margin-top: 40%;
-	margin-left: 8px;
-}
-#hfooter{
-	font-size: 8px;
-	text-align: center;
-	/*add background color here then  sololearn/code pen and github profile links*/
-}
-.icon{
-	width: 35px;
-	height: 35px;
-}
-#bar a{
-	display: inline-block;
-	vertical-align: top;
-	text-align: center;
-	margin-left: 25px;
-	margin-right: 25px;
-}
-#bar a img{
-	display: block;
-	margin: auto;
-}
-#bar a span{
-	display: block;
-}
-#settings{
-	width: 20px;
-	height: 20px;
-	position: absolute;
-	top: 0px;
-	right: 5px;
-	padding-top: 6.5px;
-}
-#stitle{
-	text-align: center;
-}
-#settingst{
-	color: white;
-	margin: 8px;
-}
-#sback{
-	position: absolute;
-	top: 0px;
-	right: 10px;
-}
-#toggle{
-	background-color:#ccc;
-	width:50px;
-	height:25px;
-	border-radius:50px;
-	box-shadow: 1px 1px 2px #666;
-}
-
-#tcircle{
-	background-color:#fff;
-	width:23px;
-	height:23px;
-	border-radius:100%;
-	position:absolute;
-	top:191px;
-	left:11.5px;
-	box-shadow:1px 1px 3px #666;
+window.onload = function(){
+    $(".div").hide();
+    var height = window.innerHeight - 52;
+    var tcolor = "default";
+	var bar = document.getElementById("bar");
+	var theme = document.getElementById("tcircle");
+	var tcircle = document.getElementById("tcircle");
+	var sinfo = document.getElementById("sinfo");
+	var settings = document.getElementById("settings");
+	var sback = document.getElementById("sback");
+	var codes = document.getElementById("codes");
+	var home = document.getElementById("home");
+	var blog = document.getElementById("project");
+	var top = document.getElementById("top");
+	var profile_pic = document.getElementById("profile_pic");
+	var htitle = document.getElementById("htitle");
+	theme.addEventListener("click", function(){
+		if(tcolor != "gray"){
+		    tcolor = "gray";
+		    		tcircle.style.left = "30px";
+			document.body.style.backgroundColor = "#f2f2f2";
+		sinfo.style.color = "black";
+		stitle.style.color = "black";
+		sback.style.color = "black";
+		tcircle.style.backgroundColor = "#212121";
+		
+		}else{
+		    tcolor = "default";
+		    sinfo.style.color = "white";
+			stitle.style.color = "white";
+			sback.style.color = "white";
+		    tcircle.style.left = "11.5px";
+			document.body.style.backgroundColor = "#212121";
+			tcircle.style.backgroundColor = "white";
+		}
+    });
+	sback.addEventListener("click", function(){
+		if(window.innerWidth > 800){
+			desktop.render();
+		}else{
+			mobile.render();
+		}
+    });
+	settings.addEventListener("click", function(){
+		$("#homet, #bar, #top, #projectt, #codet, #settings").hide();
+		$("#settingst, #sback").show();
+		document.body.style.backgroundColor = "#212121";
+	});
+	codes.addEventListener("click", function(){
+		top.innerHTML = "CODES";
+		home.style.color = "white";
+		codes.style.color = "#999999";
+		blog.style.color = "white";
+		$("#codet").show();
+		$("#homet, #projectt").hide();
+    });
+	home.addEventListener("click", function(){
+		top.innerHTML = "HOME";
+		home.style.color = "#999999";
+		codes.style.color = "white";
+		project.style.color = "white";
+		$("#homet").show();
+		$("#projectt, #codet").hide();
+    });
+	project.addEventListener("click", function(){
+		top.innerHTML = "PROJECTS";
+		home.style.color = "white";
+		codes.style.color = "white";
+		project.style.color = "#999999";
+		$("#projectt").show();
+		$("#homet, #codet").hide();
+    });
+/* mobile render */
+    function Mobile(){
+    this.render = function(){
+        $("#sback, #settingst").hide();
+        $("#bar,#top, #settings").show();
+        if(tcolor == "gray"){
+            document.body.style.backgroundColor = "#212121";
+            bar.style.backgroundColor = "#f2f2f2";
+            top.style.backgroundColor = "#f2f2f2";
+            document.body.style.color = "white";
+        }else{	document.body.style.backgroundColor = "#f2f2f2";
+            bar.style.backgroundColor = "#212121";
+            top.style.backgroundColor = "#212121";
+            document.body.style.color = "black";
+        }
+        $("#homet").show();
+        home.style.color = "#999999";
+        profile_pic.style.marginTop = "-5%";
+        bar.style.top = height + "px";
+        }
+    }
+    /* desktop render */
+    function Desktop(){
+        this.render = function(){
+            bar.style.top = height - 3 + "px";
+            if(tcolor == "gray"){
+                document.body.style.backgroundColor = "#212121";
+            }else{	
+                document.body.style.backgroundColor = "#f2f2f2";
+            }
+            $("#sback,#settingst").hide();
+            $("#bar,#top, #homet, #settings").show();
+            profile_pic.style.marginTop = "-1.8%";
+            htitle.style.marginTop = "-45%";
+        }
+    }
+    var desktop = new Desktop();
+    var mobile = new Mobile();
+    if(window.innerWidth > 800){
+        desktop.render();
+    }else{
+        mobile.render();
+    }
 }
